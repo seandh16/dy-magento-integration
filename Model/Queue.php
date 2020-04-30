f94d16635165c9f7b76c067d6484e6980fa676aa
martins.laganovskis <martins.laganovskis@magebit.com>
2020-04-30 10:28:20 +0300

    Revert "DYS-137 adjust event queue to use user sessions instead of file system"
    This reverts commit 658a7809cef19c61f66ca5084f596c171781cc15.

--- Model/Queue.php
+++ Model/Queue.php
@@ -2,11 +2,12 @@
 
 namespace DynamicYield\Integration\Model;
 
-use Magento\Customer\Model\Session;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem\Io\File;
 
 class Queue
 {
-    const COLLECTION_ID = 'dyi_queue';
+    const FILE_NAME = 'dyi_queue.json';
 
     /**
      * @var array
@@ -14,20 +15,28 @@ class Queue
     protected $_collection = [];
 
     /**
-     * @var Session
+     * @var DirectoryList
      */
-    protected $_session;
+    protected $_directoryList;
+
+    /**
+     * @var File
+     */
+    protected $_file;
 
     /**
      * Queue constructor
      *
-     * @param Session $session
+     * @param DirectoryList $directoryList
+     * @param File $file
      */
     public function __construct(
-        Session $session
+        DirectoryList $directoryList,
+        File $file
     )
     {
-        $this->_session = $session;
+        $this->_directoryList = $directoryList;
+        $this->_file = $file;
     }
 
     /**
@@ -35,7 +44,7 @@ class Queue
      */
     public function getCollection()
     {
-        return json_decode($this->_session->getData(self::COLLECTION_ID),true);
+        return json_decode($this->_file->read($this->getFile()), true);
     }
 
     /**
@@ -43,7 +52,7 @@ class Queue
      */
     public function updateCollection()
     {
-        return $this->_session->setData(self::COLLECTION_ID, json_encode($this->_collection));
+        return $this->_file->write($this->getFile(), json_encode($this->_collection));
     }
 
     /**
@@ -52,8 +61,16 @@ class Queue
      */
     public function addToQueue(array $data)
     {
-        $this->_collection = $this->getCollection();
-        $this->_collection[] = $data;
+        $collection = $this->getCollection();
+
+        if (!isset($data['session_id'])) {
+            $data['session_id'] = session_id();
+        }
+
+        $collection[] = $data;
+
+        $this->_collection = array_unique($collection, SORT_REGULAR);
+
         return $this->updateCollection();
     }
 
@@ -63,6 +80,26 @@ class Queue
     public function clearQueue()
     {
         $this->_collection = [];
+
         return $this->updateCollection();
+    }
+
+    /**
+     * @return string
+     */
+    protected function getFile()
+    {
+        $path = $this->_directoryList->getPath(DirectoryList::VAR_DIR);
+        $file = $path . '/' . self::FILE_NAME;
+
+        $this->_file->open([
+            'path' => $path
+        ]);
+
+        if (!$this->_file->fileExists($file)) {
+            $this->_file->write($file, json_encode($this->_collection), 0666);
+        }
+
+        return $file;
     }
 }
